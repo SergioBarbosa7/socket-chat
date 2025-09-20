8581120852cae4b836f9cf5e2905f3b12c422a29
sergio.natan <sergionatan.01@gmail.com>
2025-09-20 10:25:24 -0300

    Initial Commit

---
+++ src/test/java/br/com/study/socketchat/SocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.socketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
